5138e886297ae75877ebd256dd3a49afe638078a
mofumofuchan <you_oki1@yahoo.co.jp>
2017-08-02 01:56:17 +0900

    make .emacs.d if it doesn't exist

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+if [ ! -e "$HOME/.emacs.d" ]; then
+  echo "create $HOME/.emacs.d"
+  mkdir "$HOME/.emacs.d"
+fi
+
+echo "make a symbolic link to dotfiles/init.el"
 ln -s "$(pwd)/init.el" "$HOME/.emacs.d/init.el"
+
