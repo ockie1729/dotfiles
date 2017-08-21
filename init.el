ba1ef1c6ee0e507ea2ec1b021d4f84baaced44aa
mofumofuchan <you_oki1@yahoo.co.jp>
2017-08-21 17:08:38 +0900

    Add Gauche settings

--- init.el
+++ init.el
@@ -207,5 +207,21 @@
 (add-hook 'yatex-mode-hook
           '(lambda ()
              (reftex-mode 1)
-             (define-key reftex-mode-map (concat YaTeX-prefix ">") 'YaTeX-comment-region)
+             (define-key reftex
+	       -mode-map (concat YaTeX-prefix ">") 'YaTeX-comment-region)
              (define-key reftex-mode-map (concat YaTeX-prefix "<") 'YaTeX-uncomment-region)))
+
+;; Gauche
+(setq scheme-program-name "gosh -i")
+(autoload 'scheme-mode "cmuscheme" "Major mode for Scheme." t)
+(autoload 'run-scheme "cmuscheme" "Run an inferior Scheme process." t)
+
+(defun scheme-other-window ()
+  "Run scheme on other window"
+  (interactive)
+  (switch-to-buffer-other-window
+   (get-buffer-create "*scheme*"))
+  (run-scheme scheme-program-name))
+
+(define-key global-map
+  "\C-cs" 'scheme-other-window)
