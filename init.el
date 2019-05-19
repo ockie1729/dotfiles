7951b8b59821d8f737a32084b4ec55454227cbb0
mofumofuchan <you_oki1@yahoo.co.jp>
2019-05-19 23:20:23 +0900

    Add recentf

--- init.el
+++ init.el
@@ -19,6 +19,7 @@
     stan-mode
     yaml-mode
     julia-mode
+    recentf-ext
    ))
 
 ;; my/favorite-packagesからインストールしていないパッケージをインストール
@@ -260,3 +261,21 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+;; recentf
+;; ref: http://keisanbutsuriya.hateblo.jp/entry/2015/02/15/174758
+
+(defmacro with-suppressed-message (&rest body)
+  "Suppress new messages temporarily in the echo area and the `*Messages*' buffer while BODY is evaluated."
+  (declare (indent 0))
+  (let ((message-log-max nil))
+    `(with-temp-message (or (current-message) "") ,@body)))
+
+(require 'recentf)
+(setq recentf-save-file "~/.emacs.d/.recentf")
+(setq recentf-max-saved-items 1000)            ;; recentf に保存するファイルの数
+(setq recentf-exclude '(".recentf"))           ;; .recentf自体は含まない
+(setq recentf-auto-cleanup 'never)             ;; 保存する内容を整理
+(run-with-idle-timer 30 t '(lambda ()          ;; 30秒ごとに .recentf を保存
+   (with-suppressed-message (recentf-save-list))))
+(require 'recentf-ext)
+(global-set-key (kbd "C-c o") 'recentf-open-files)
