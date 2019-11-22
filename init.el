310bc7a8bdb25632cd83f367ae1fa7bc654b38f1
mofumofuchan <you_oki1@yahoo.co.jp>
2019-11-22 16:19:29 +0900

    Add yasnippet package

--- init.el
+++ init.el
@@ -20,6 +20,8 @@
     yaml-mode
     julia-mode
     recentf-ext
+    yasnippet
+    yasnippet-snippets
    ))
 
 ;; my/favorite-packagesからインストールしていないパッケージをインストール
@@ -289,3 +291,17 @@
    (with-suppressed-message (recentf-save-list))))
 (require 'recentf-ext)
 (global-set-key (kbd "C-c o") 'recentf-open-files)
+
+;;
+;; yasnippet
+;;
+(require 'yasnippet)
+
+;; 既存スニペットを挿入する
+(define-key yas-minor-mode-map (kbd "C-x i i") 'yas-insert-snippet)
+;; 新規スニペットを作成するバッファを用意する
+(define-key yas-minor-mode-map (kbd "C-x i n") 'yas-new-snippet)
+;; 既存スニペットを閲覧・編集する
+(define-key yas-minor-mode-map (kbd "C-x i v") 'yas-visit-snippet-file)
+
+(yas-global-mode 1)
