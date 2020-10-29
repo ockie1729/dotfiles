338a3702e94c6d2e5e8eee118193c2d34d2bff94
Yusuke Okimoto <yusokimo@yahoo-corp.jp>
2020-10-29 13:27:08 +0900

    pythonのテンプレートで用いている引用符をシングルに変更

--- insert/template.py
+++ insert/template.py
@@ -5,7 +5,7 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--tmp", type=str, required=True)
+    parser.add_argument('--tmp', type=str, required=True)
     args = parser.parse_args()
 
 
